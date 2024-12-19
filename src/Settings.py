e128de2418d627383127cda192f50a519e4a0611
blackat5445 <kfalahati49@gmail.com>
2024-12-19 16:41:26 +0100

    new Order modality b to t and t to b

--- src/Settings.py
+++ src/Settings.py
@@ -1,10 +1,10 @@
-from colorama import Fore, Style, init
-from .. import main
+from colorama import Fore, Style, init, just_fix_windows_console
+from .utils import save_settings, load_settings
 
 def settings():
     """Manage Spotify settings and multi-playlist mode."""
     print(Fore.CYAN + "\n--- Settings ---")
-    settings_data =  main.py.load_settings()
+    settings_data = load_settings()
 
     if settings_data["client_id"] and settings_data["client_secret"]:
         print("Spotify credentials already exist.")
@@ -21,6 +21,15 @@ def settings():
 
     multi_playlist_mode = input("Enable multi-playlist mode? (yes/no): ").strip().lower() == "yes"
 
-    main.py.save_settings(client_id, client_secret, multi_playlist_mode)
+    print(
+        Fore.YELLOW +
+        "\nIt's better to use bottom-to-top if you want to maintain the same order.\n"
+    )
+    reorder_direction = input(
+        "Choose reordering direction:\n1 - Bottom-to-Top (default)\n2 - Top-to-Bottom\nEnter your choice (1/2): "
+    ).strip()
+    reorder_direction = "bottom-to-top" if reorder_direction != "2" else "top-to-bottom"
+
+    save_settings(client_id, client_secret, multi_playlist_mode, reorder_direction)
     print(Fore.GREEN + "\nSettings saved successfully!")
     input("\nPress Enter to return to the menu.")
