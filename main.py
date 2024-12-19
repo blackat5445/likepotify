388a08d0e5691e67aa50f28a6c0af2e525303fea
blackat5445 <kfalahati49@gmail.com>
2024-12-19 16:42:25 +0100

    fixed the Circular Imports fixed the color bug some rainbow added :D

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import os
 import webbrowser
 import json
 import winsound
-from colorama import Fore, Style, init
+from colorama import Fore, Style, init, just_fix_windows_console
 import src.Settings
 import src.LikedSongsOperations
-
+import src.utils
+just_fix_windows_console()
 init()
 
 # Constants
@@ -19,29 +20,12 @@ PLAYLIST_FILE = os.path.join(maindir, r'playlists/Import Playlists/import.txt')
 TUTORIAL_URL = "https://www.example.com/tutorial"  # Replace with your tutorial URL
 REORDER_OPERATION = src.LikedSongsOperations
 SETTINGS_OPERATION = src.Settings
+UTILITY = src.utils
 
 
 #helper function to clear CUI
 def screen_clear():
     _ = os.system('cls')
-
-def save_settings(client_id, client_secret, multi_playlist_mode):
-    """Save Spotify credentials and settings to a JSON file."""
-    with open(SETTINGS_FILE, "w") as file:
-        json.dump({
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "multi_playlist_mode": multi_playlist_mode
-        }, file)
-
-
-def load_settings():
-    """Load Spotify credentials and settings from a JSON file."""
-    if os.path.exists(SETTINGS_FILE):
-        with open(SETTINGS_FILE, "r") as file:
-            return json.load(file)
-    return {"client_id": "", "client_secret": "", "multi_playlist_mode": False}
-
 
 def display_banner():
     """Display the banner with thick and random art."""
@@ -53,17 +37,18 @@ def display_banner():
 
 def about():
     """Display information about the program."""
-    print("\n--- About ---")
-    print("Likepotify: A tool to manage your Spotify liked songs.")
-    print("Version: 1.0")
-    print("Developed by: Kasra Falahati")
+    print(Fore.LIGHTGREEN_EX + "\n--- About ---")
+    print(Fore.LIGHTBLACK_EX + "Likepotify: A tool to transfer your play list to liked songs.")
+    print(Fore.YELLOW + "Version: 1.0")
+    print(Fore.LIGHTRED_EX + "Developed by: KASRA FALAHATI")
+    print(Fore.CYAN + "Sponsored by: WWW.AGENZIAMAGMA.IT")
     input("\nPress Enter to return to the menu.")
     screen_clear()
 
 
 def tutorial():
     """Redirect to the tutorial page."""
-    print("Please wait while we are opening the tutorial...")
+    print(Fore.LIGHTGREEN_EX + "Please wait while we are opening the tutorial...")
     time.sleep(2)
     webbrowser.open(TUTORIAL_URL)
     print("\nTutorial opened. Returning to menu...")
@@ -74,13 +59,15 @@ def tutorial():
 
 def start():
     """Start the Spotify reordering process."""
-    settings_data = load_settings()
+    settings_data = UTILITY.load_settings()
     client_id = settings_data.get("client_id")
     client_secret = settings_data.get("client_secret")
     multi_playlist_mode = settings_data.get("multi_playlist_mode", False)
+    reorder_direction = settings_data.get("reorder_direction", "bottom-to-top")
 
     if not client_id or not client_secret:
         print(Fore.RED + "\nSpotify credentials are missing. Please configure them in the Settings menu first.")
+        print(Fore.YELLOW + "\nIf you need to import more than 1 playlist you can fill the Import Playlists/imports.txt")
         input("\nPress Enter to return to the menu.")
         return
 
@@ -90,7 +77,7 @@ def start():
             playlist_urls = [line.strip() for line in file if line.strip()]
             for playlist_url in playlist_urls:
                 print(Fore.YELLOW + f"\nProcessing playlist: {playlist_url}")
-                REORDER_OPERATION.reorder_liked_songs_from_playlist(playlist_url, client_id, client_secret)
+                REORDER_OPERATION.reorder_liked_songs_from_playlist(playlist_url, client_id, client_secret, reorder_direction)
     else:
         playlist_url = input("\nEnter the Spotify playlist link: ").strip()
         if not playlist_url:
@@ -99,7 +86,7 @@ def start():
             return
 
         print(Fore.GREEN + "\nProcessing playlist. This may take a while...")
-        REORDER_OPERATION.reorder_liked_songs_from_playlist(playlist_url, client_id, client_secret)
+        REORDER_OPERATION.reorder_liked_songs_from_playlist(playlist_url, client_id, client_secret, reorder_direction)
 
     input(Fore.GREEN + "\nReordering complete! Press Enter to return to the menu.")
     screen_clear()
@@ -135,11 +122,11 @@ def menu():
 
 if __name__ == "__main__":
     # Load settings at the start of the program
-    settings_data = load_settings()
+    settings_data =  UTILITY.load_settings()
     if settings_data["client_id"] and settings_data["client_secret"]:
-        print("Settings loaded successfully.")
+        print(Fore.GREEN + "Settings loaded successfully.")
     else:
-        print("No settings found. Please configure your Spotify credentials in the Settings menu.")
+        print(Fore.RED + "No settings found. Please configure your Spotify credentials in the Settings menu.")
 
     # Start the menu
     menu()
