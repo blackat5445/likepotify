f6be60eea0bbd92b20b0df2d91224f0b947d326c
blackat5445 <kfalahati49@gmail.com>
2024-12-20 17:23:50 +0100

    trying to fix exe

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,16 @@
 import json
 import os
-
+import sys
 # Constants
-SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../settings.json')
+def get_base_path():
+    """Determine the base path for resources."""
+    if hasattr(sys, "_MEIPASS"):  # Check if running from PyInstaller executable
+        return sys._MEIPASS
+    return os.path.dirname(os.path.abspath(__file__))  # Development environment
+
+SETTINGS_FILE = os.path.join(get_base_path(), "settings.json")
+
+
 
 def save_settings(client_id, client_secret, multi_playlist_mode=False, reorder_direction="bottom-to-top"):
     """Save Spotify credentials and settings to a JSON file."""
